50d9df46698ac0a971b401fce7bebae59394f5aa
Andew <54282427+andreyyarchuk@users.noreply.github.com>
2023-07-17 22:47:31 +0200

    LeetCode: Palindrome Number.
    Given an integer x, return true if x is a  palindrome , and false otherwise.

---
+++ LC2.py
@@ -0,0 +1,22 @@
+#   Palindrome Number
+# Given an integer x, return true if x is a
+# palindrome,
+# and false otherwise
+
+def isPalindrome(x):
+    str_num = str(x)
+    new_num = ''
+    for i in range(len(str_num), 0, -1):
+        new_num += str_num[i-1]
+    if new_num == str_num:
+        return True
+    else:
+        return False
+
+def isPalindrome2(x):
+    str_num = str(x)
+    new_num = str_num[::-1]
+    if new_num == str_num:
+        return True
+    else:
+        return False
